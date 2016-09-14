5be4791c1f53b6ace6e677bf5eeec91ca4bc10a0
Devan <devanpatel13@gmail.com>
2016-09-14 14:33:49 -0400

    Starting networking..

---
+++ KSU_NETWORK_SOLUTION/Assets/Scripts/Network/GameNetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+public class GameNetworkManager : NetworkManager   {
+
+	void Start ()
+	{
+		StartServer();
+	}
+
+	void Update ()
+	{
+
+	}
+
+	public void OnClientConnect(NetworkConnection conn)
+	{
+		Debug.Log(conn);
+	}
+
+
+
+}
